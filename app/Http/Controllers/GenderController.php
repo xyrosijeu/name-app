32a2f78d2d2c04e1e0788d8f6608ad9b4b4b9181
xyrosijeu <xyrosijeu@gmail.com>
2024-03-12 11:59:23 +0800

    dghasjkdhkjl

--- app/Http/Controllers/GenderController.php
+++ app/Http/Controllers/GenderController.php
@@ -6,5 +6,35 @@ use Illuminate\Http\Request;
 
 class GenderController extends Controller
 {
-    
+    public function index () {
+        return view('gender.index');
+    }
+
+    public function show () {
+
+    }
+
+    public function create () {
+
+    } 
+
+    public function store () {
+
+    }
+
+    public function edit () {
+
+    }
+
+    public function update () {
+
+    }
+
+    public function delete () {
+
+    }
+
+    public function destroy () {
+
+    }
 }
